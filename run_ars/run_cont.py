11649da11cd930139f00316c53ec4736f90fc821
sgillen <sgillen@ucsb.edu>
2020-07-27 18:51:32 -0700

    fallback commit

--- run_ars/run_cont.py
+++ run_ars/run_cont.py
@@ -2,36 +2,26 @@ from seagul.rl.ars.ars_pipe import ars
 from seagul.nn import MLP
 import torch
 import matplotlib.pyplot as plt
-from seagul.mesh import variation_dim
-from seagul.mesh import mesh_dim
+from seagul.mesh import create_mesh, variation_dim, mesh_dim, create_box_mesh
 import time
 import copy
 import gym
+import torch
+
 import xarray as xr
 import numpy as np
 import pandas as pd
+
 import os
-import seagul.envs
-from seagul.integration import euler
-from sklearn.decomposition import PCA
 
 def identity(rews,obs,acts):
     return rews
 
-def madodiv(rews, obs, acts):
-    return rews/variation_dim(obs, order=1)
+def vardiv(rews, obs, acts):
+    return rews/variation_dim(obs)
 
-def variodiv(rews, obs, acts):
-    return rews/variation_dim(obs, order=2)
-
-def radodiv(rews, obs, acts):
-    return rews/variation_dim(obs, order=.5)
-
-# def vardiv(rews, obs, acts):
-#     return rews/variation_dim(obs)
-
-# def varmul(rews, obs, acts):
-#     return rews*variation_dim(obs)
+def varmul(rews, obs, acts):
+    return rews*variation_dim(obs)
 
 def rough_measure(x):
     d = torch.stack([x[t,:] - x[t-1,:] for t in range(1,x.shape[0])])
@@ -48,6 +38,7 @@ def pcastd(rews, obs, acts):
     pca.fit((obs - obs.mean())/obs.std())
     return rews*pca.explained_variance_ratio_.std()
 
+
 def mdim_mul(rews, obs, acts):
     m,_,_,_ = mesh_dim(obs)
     return m*rews
@@ -61,9 +52,28 @@ def cdim_mul(rews, obs, acts):
     return c*rews
 
 def cdim_div(rews, obs, acts):
-    _,c,_,_ = mesh_dim(obs)
+
+    if obs.shape[0] == 1000:
+        gait_start = 200
+    else:
+        gait_start = 0
+
+    _,c,_,_ = mesh_dim(obs[gait_start:])
+    c = np.clip(c, 0, len(rews))
     return rews/c
+
+
    
+
+
+def madodiv(rews, obs, acts):
+    return rews/variation_dim(obs, order=1)
+
+def variodiv(rews, obs, acts):
+    return rews/variation_dim(obs, order=2)
+
+def radodiv(rews, obs, acts):
+    return rews/variation_dim(obs, order=.5)
 
 
 
@@ -71,41 +81,23 @@ env_names = ["Walker2d-v2"]#,  "Hopper-v2", "HalfCheetah-v2"]#,  "linear_z2d-v0"
 post_fns = [cdim_div, mdim_div]
 #post_fns = [cdim_div]
 
+
 torch.set_default_dtype(torch.float64)
 num_experiments = len(post_fns)
-num_seeds = 5
+num_seeds = 2
 num_epochs = 250
-n_workers = 24; n_delta = 60; n_top = 20; exp_noise=.025
+n_workers = 12; n_delta = 60; n_top = 20; exp_noise=.025
 
-save_dir = "./datacdi2/"
+init_data = torch.load("./data17/Walker2d-v2.xr")
+init_policy_dict = init_data.policy_dict
+
+save_dir = "./datacdim2/"
 os.makedirs(save_dir)
 
 import time
 start = time.time()
 
-
 env_config = {}
-
-
-# def reward_fn_2d(s):
-#     if s[2] > 0:
-#         if s[0] >= 0 and s[1] >= 0:
-#             reward = np.clip(np.sqrt(s[0]**2 + s[1]**2),0,10)
-#             #reward = 5 - np.clip(np.abs(np.sqrt(s[0]**2 + s[2]**2) - 5)**2,0,5)
-#             s[2] = -10
-#         else:
-#             reward = 0.0
-
-#     elif s[2] < 0:
-#         if s[0] <= 0 and s[1] <= 0:
-#             reward = np.clip(np.sqrt(s[0]**2 + s[1]**2),0,10)
-#             #reward = 5 - np.clip(np.abs(np.sqrt(s[0]**2 + s[2]**2)**2 - 5),0,5)
-#             s[2] = 10
-#         else:
-#             reward = 0.0
-
-#     return reward, s
-
 
 for env_name in env_names:
         env = gym.make(env_name, **env_config)
@@ -131,12 +123,12 @@ for env_name in env_names:
             
 
         for post_fn in post_fns:
-            for i in range(num_seeds):
-                policy = MLP(in_size,out_size,0,0)
-                policy, r_hist, lr_hist = ars(env_name, policy, num_epochs, n_workers=n_workers, n_delta=n_delta, n_top=n_top, exp_noise=exp_noise, postprocess=post_fn, env_config=env_config)
+            for i in [1]:#range(num_seeds):
+                policy = init_policy_dict['identity'][i]
+                policy, r_hist, lr_hist = ars(env_name, policy, num_epochs, n_workers=n_workers, n_delta=n_delta, n_top=n_top, exp_noise=exp_noise, postprocess=post_fn, env_config=env_config, learn_means=False, zero_policy=False)
                 print(f"{env_name}, {post_fn.__name__}, {i}, {time.time() - start}")
                 data.policy_dict[post_fn.__name__].append(copy.deepcopy(policy))
-                data.rews.loc[post_fn.__name__,i,:] = lr_hist
-                data.post_rews.loc[post_fn.__name__,i,:] = r_hist
+                data.rews.loc[post_fn.__name__, i, :] = lr_hist
+                data.post_rews.loc[post_fn.__name__, i ,:] = r_hist
 
         torch.save(data, f"{save_dir}{env_name}.xr")
